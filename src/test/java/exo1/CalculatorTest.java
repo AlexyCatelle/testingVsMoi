374cb05c0f6c6e2ea4b8b6916b4072ef54c32ada
AlexyCatelle <alexy.catelle@oclock.school>
2025-09-25 11:06:37 +0200

    test error ok

--- src/test/java/exo1/CalculatorTest.java
+++ src/test/java/exo1/CalculatorTest.java
@@ -1,4 +1,17 @@
+package exo1;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    @Test
+    @DisplayName("addition")
+    void testAdd_OnePlusTwo_ShouldReturntree() {
+        Calculator calculator = new Calculator();
+
+        double result = calculator.add(1, 2);
+        assertEquals(4, result);
+    }
+};
