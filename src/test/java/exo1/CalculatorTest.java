118c6af1fb34610fc2842afe83400fa59f3b66ac
AlexyCatelle <alexy.catelle@oclock.school>
2025-09-25 11:14:20 +0200

    test add ok

--- src/test/java/exo1/CalculatorTest.java
+++ src/test/java/exo1/CalculatorTest.java
@@ -1,17 +1,28 @@
 package exo1;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CalculatorTest {
+@TestMethodOrder(MethodOrderer.DisplayName.class)
+class CalculatorTest {
+
+    @Test
+    @DisplayName("Error add")
+    void testAddError(){
+        Calculator calculator = new Calculator();
+
+        double result = calculator.add(1,2);
+        assertNotEquals(3,result);
+    }
+
+
     @Test
     @DisplayName("addition")
     void testAdd_OnePlusTwo_ShouldReturntree() {
         Calculator calculator = new Calculator();
 
         double result = calculator.add(1, 2);
-        assertEquals(4, result);
+        assertEquals(3, result);
     }
-};
+}
